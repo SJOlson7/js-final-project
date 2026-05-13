6778aa510fc4a380124183867140fa9adf84114e
Kaylee Frank <frankaylee10@gmail.com>
2026-05-13 17:06:09 -0500

    edited menu/event pages, added form functionality

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,8 +1,10 @@
 
 (async () => {
 
-    const menuContainer = document.querySelector('#menu-item')
-    const eventContainer = document.querySelector('#event-name')
+const menuContainer = document.querySelector('#menu-item')
+const eventContainer = document.querySelector('#event-name')
+
+if (menuContainer) {
 
     const menuResult = await fetch('/api/v1/menu')
     const menu = await menuResult.json()
@@ -13,10 +15,14 @@
             "<p>" + "<img src='" + item.photo + "' width='200'><br>" +
                 "<strong>" + item.name + "</strong><br>" +
                 item.description + "<br>" +
-                "<strong>$" + item.price + "</strong>"
+                "<strong>$" + item.price + "</strong>" +
             "</p>"
 
     })
+}
+
+
+if (eventContainer) {
 
     const eventResult = await fetch('/api/v1/events')
     const events = await eventResult.json()
@@ -31,5 +37,66 @@
             "</p>"
 
     })
+}
+
+
+const menuForm = document.querySelector('#menu-form')
+
+if (menuForm) {
+
+    menuForm.addEventListener('submit', async (event) => {
+
+        event.preventDefault()
+
+        const data = {
+            name: document.querySelector('#menuname-input').value,
+            description: document.querySelector('#menudescription-input').value,
+            price: document.querySelector('#menuprice-input').value,
+            photo: document.querySelector('#menuphoto-input').value
+        }
+
+        await fetch('/api/v1/menu', {
+            method: 'POST',
+            headers: {  'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+    alert('Menu item added successfully!')
+
+    menuForm.reset()
+
+    })
+}
+
+
+const eventForm = document.querySelector('#event-form')
+
+if (eventForm) {
+
+    eventForm.addEventListener('submit', async (event) => {
+
+        event.preventDefault()
+
+        const data = {
+            event: document.querySelector('#eventname-input').value,
+            date: document.querySelector('#eventdate-input').value,
+            time: document.querySelector('#eventtime-input').value,
+            venue: document.querySelector('#eventvenue-input').value,
+            location: document.querySelector('#eventlocation-input').value
+        }
+
+        await fetch('/api/v1/events', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+    alert('Event added successfully!')
+
+    eventForm.reset()
+
+    })
+}
+
 
 })()
