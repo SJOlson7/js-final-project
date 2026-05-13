86b49033955f861da22ff8021a0cb977819a0a7c
Kaylee Frank <frankaylee10@gmail.com>
2026-05-13 14:05:30 -0500

    added menu and event api routes

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -1,13 +1,34 @@
 const router = require('express').Router()
-const { getCollection, ObjectId } = require('../../../dbconnect.js')
+const { getCollection, ObjectId } = require('../../../dbconnect')
 
 let collection = null
-const getMenu = async () => {
-    if (!collection) collection = await getCollection('Mais-ThaiAPI', 'Events')
+
+const getEvents = async () => {
+    if (!collection) {collection = await getCollection('mais-thai', 'Events')}
     return collection
 }
 
+router.get('/', async (request, response) => {
+    const collection = await getEvents()
+    const found = await collection.find().toArray()
+    response.send(found)
+})
 
+router.get('/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getEvents()
+    const found = await collection.findOne({_id: new ObjectId(id)})
 
+    if (found) {response.send(found)}
+    else { response.send({ error: { message: `Could not find event with id: ${id}` } })  }
+})
+
+router.post('/', async (request, response) => {
+    const { title, location, date, description, image } = request.body
+    const collection = await getEvents()
+    const { acknowledged, insertedId } = await collection.insertOne({ title, location, date, description, image })
+
+    response.send({ acknowledged, insertedId })
+})
 
 module.exports = router
--- routes/api/v1/menu.js
+++ routes/api/v1/menu.js
@@ -1,14 +1,34 @@
 const router = require('express').Router()
-const { getCollection, ObjectId } = require('../../../dbconnect.js')
+const { getCollection, ObjectId } = require('../../../dbconnect')
 
 let collection = null
+
 const getMenu = async () => {
-    if (!collection) collection = await getCollection('Mais-ThaiAPI', 'Menu')
+    if (!collection) {collection = await getCollection('mais-thai', 'Menu')}
     return collection
 }
 
+router.get('/', async (request, response) => {
+    const collection = await getMenu()
+    const found = await collection.find().toArray()
+    response.send(found)
+})
 
+router.get('/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getMenu()
+    const found = await collection.findOne({_id: new ObjectId(id)})
 
+    if (found) {response.send(found)}
+    else { response.send({ error: { message: `Could not find menu item with id: ${id}`} }) }
+})
 
+router.post('/', async (request, response) => {
+    const { name, description, price, image } = request.body
+    const collection = await getMenu()
+    const { acknowledged, insertedId } = await collection.insertOne({ name, description, price, image })
+
+    response.send({ acknowledged, insertedId })
+})
 
 module.exports = router
